5ecda9679ee9b01d5755e1873a8a9c136628c567
Mümin Yıldırım <mmnyldrm61@gmail.com>
2023-01-10 00:43:23 +0300

    build

--- hooks/useFetcher.tsx
+++
@@ -1,9 +0,0 @@
-import { fetcher } from "@/helpers/fetcher"
-import { UseFetchPropType } from "@/types/hooks"
-import useSWR from "swr"
-
-export default function useFetcher<T>({ url }: UseFetchPropType) {
-  const { data, error, isLoading } = useSWR<T>(url, fetcher)
-
-  return { data, error, isLoading }
-}
