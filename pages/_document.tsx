2a26666f9ac9ff6530c6c90b10bd4e73e67010da
Mümin Yıldırım <mmnyldrm61@gmail.com>
2023-01-14 14:20:43 +0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,20 +3,26 @@ import { Html, Head, Main, NextScript } from "next/document"
 export default function Document() {
   return (
     <Html lang="en" className="dark">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-JYX9TG6V24"
+        ></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-          `,
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-JYX9TG6V24');
+        `,
           }}
         />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
       </body>
     </Html>
   )
