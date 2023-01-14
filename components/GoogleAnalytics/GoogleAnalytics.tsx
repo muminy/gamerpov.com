37f7c6c629f7b2e6d680d365b544bcf9851309da
Mümin Yıldırım <mmnyldrm61@gmail.com>
2023-01-14 14:32:52 +0300

    ga

--- components/GoogleAnalytics/GoogleAnalytics.tsx
+++ components/GoogleAnalytics/GoogleAnalytics.tsx
@@ -1,46 +1,39 @@
 import { useRouter } from "next/router"
 import Script from "next/script"
 import { useEffect } from "react"
+import { pageview } from "@/libs/ga"
 
-const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS
+const GA_TRACKING_ID = "G-JYX9TG6V24"
 
 export default function GoogleAnalytics() {
   const router = useRouter()
 
   useEffect(() => {
     const handleRouteChange = (url: string) => {
-      gtag("config", GA_TRACKING_ID!, {
-        page_path: url,
-      })
+      pageview(url, GA_TRACKING_ID)
     }
+
     router.events.on("routeChangeComplete", handleRouteChange)
-    router.events.on("hashChangeComplete", handleRouteChange)
     return () => {
       router.events.off("routeChangeComplete", handleRouteChange)
-      router.events.off("hashChangeComplete", handleRouteChange)
     }
   }, [router.events])
 
   return (
     <>
       <Script
-        strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
         strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${GA_TRACKING_ID}', {
-          page_path: window.location.pathname,
-        });
-      `,
-        }}
       />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${GA_TRACKING_ID}');
+        `}
+      </Script>
     </>
   )
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,23 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document"
 export default function Document() {
   return (
     <Html lang="en" className="dark">
-      <Head>
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-JYX9TG6V24"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'G-JYX9TG6V24');
-        `,
-          }}
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
